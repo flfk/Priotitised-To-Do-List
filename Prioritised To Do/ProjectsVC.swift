3bdbd2d0df63cf5dc4d8686a7871ed7230835ee2
flfk <flfk@users.noreply.github.com>
2017-06-10 21:32:16 -0300

    updated projects VC

--- Prioritised To Do/ProjectsVC.swift
+++ Prioritised To Do/ProjectsVC.swift
@@ -10,20 +10,39 @@ import UIKit
 
 class ProjectsVC: UIViewController {
 
+    //MARK: - Properties
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var newProjectTextField: UITextField!
+    
+    //MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
     
     // MARK: - Navigation
 
     //MARK: - Action
     
     @IBAction func saveNewProjectButton(_ sender: Any) {
+    }
+}
+
+extension ProjectsVC: UITextFieldDelegate {
+    
+    //don't forget in the storyboard to set the textfield delegate to the viewcontroller
+    
+    //hide keyboard when the user touches outside keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //hide keyboard when the user touches return key
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        newProjectTextField.resignFirstResponder()
+        return true
     }
 }
